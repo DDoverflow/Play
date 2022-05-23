5dcc181ce2e390de21366053de1eff275d38682a
StriveDD <Kip2021czb@gmail.com>
2022-05-23 19:18:46 +0800

    Modified some files

--- Assignment/Task4.cpp
+++ Assignment/Task4.cpp
@@ -2,10 +2,38 @@
 #include <vector>
 using namespace std;
 
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        vector<vector<int>> dp(s.length() + 1, vector<int>(t.length() + 1, 0));
+        for (int num = 0; num <= s.length(); num++) dp[num][0] = 1;
+        for (int line = 1; line <= s.length(); line++) {
+            for (int row = 1; row <= t.length(); row++) {
+                if (s[line - 1] == t[row - 1]) dp[line][row] = dp[line - 1][row - 1] + dp[line - 1][row];
+                else dp[line][row] = dp[line - 1][row];
+            }
+        }
+        for (int num1 = 0; num1 <= s.length(); num1++) {
+            for (int num2 = 0; num2 <= t.length(); num2++)
+                cout << dp[num1][num2] << "  ";
+            cout << endl;
+        }
+        return dp[s.length()][t.length()];
+    }
+};
+
+/*
+     b  a  g
+  1  0  0  0
+b 1  1  0  0
+a 1  1  1  0
+g 1  1  1  1  
+g 1  1  1  2
+ */
+
 int main() {
-    string matter = "0";
-    for (int num = 1; num <= 3; num++)
-        matter.insert(matter.begin(), '1');
-    cout << matter << endl;
+    Solution matter;
+    string s = "bagg", t = "bag";
+    matter.numDistinct(s, t);
     return 0;
 }
--- Assignment/Task5.cpp
+++ Assignment/Task5.cpp
@@ -1,36 +1,94 @@
 #include <iostream>
+#define LH 2
+#define RH -2
 using namespace std;
-#include <cmath>
 #include <vector>
-#include <unordered_map>
 
-class Solution {
-public:
-    unordered_map<int, int> map;
-    vector<int> result;
-    int Max = 0;
-
-    vector<int> findRightInterval(vector<vector<int>>& intervals) {
-        if (intervals.size() == 1) return vector<int>{-1};
-        for (int num = 0; num < intervals.size(); num++) {
-            map[intervals[num][0]] = num;
-            Max = max(Max, intervals[num][0]);
-        }
-        for (int num = 0; num < intervals.size(); num++) {
-            int middle = intervals[num][1];
-            while (middle) {
-                if (map[middle] != 0) result.push_back(map[middle]);
-                else if (middle < Max) middle++;
-                else {result.push_back(-1);  break;}
-            }
-        }
-        return result;
-    }
+struct Node {
+    int data;
+    struct Node *left = nullptr;
+    struct Node *right = nullptr;
+    Node() {}
+    Node(int data): data(data) {}
 };
 
+struct Tree {
+    struct Node *root = nullptr;
+};
+
+int Acquire_BF(struct Node *&matter, int count = 1) {
+    if (matter == nullptr) return 0;
+    if (matter->left == nullptr && matter->right == nullptr) return 1;
+    int left = Acquire_BF(matter->left, count + 1);
+    int right = Acquire_BF(matter->right, count + 1);
+    if (count == 1) return left - right;
+    return max(left, right) + 1;
+}
+
+void Left_rotate(struct Node *&matter) {
+    struct Node *matter_right = matter->right;
+    matter->right = matter_right->left;
+    matter_right->left = matter;
+    matter = matter_right;
+}
+
+void Right_rotate(struct Node *&matter) {
+    struct Node *matter_left = matter->left;
+    matter->left = matter_left->right;
+    matter_left->right = matter;
+    matter = matter_left;
+}
+
+void Left_balance(struct Node *&matter) {
+    struct Node *matter_left = matter->left;
+    switch (Acquire_BF(matter_left)) {
+        case LH - 1: {Right_rotate(matter);  break;}
+        case RH + 1: {Left_rotate(matter->left);  Right_rotate(matter);  break;}
+    }
+}
+
+void Right_balance(struct Node *&matter) {
+    struct Node *matter_right = matter->right;
+    switch (Acquire_BF(matter_right)) {
+        case LH - 1: {Right_rotate(matter->right);  Left_rotate(matter);  break;}
+        case RH + 1: {Left_rotate(matter);  break;}
+    }
+}
+
+void Create_AVL(struct Node *&matter, int data) {
+    if (matter == nullptr) {
+        struct Node *contents = new Node(data);
+        matter = contents;
+        return;
+    }
+    if (data >= matter->data) Create_AVL(matter->right, data);
+    else Create_AVL(matter->left, data);
+    if (Acquire_BF(matter) == LH) Left_balance(matter);
+    else if (Acquire_BF(matter) == RH) Right_balance(matter);
+}
+
+void Preorder(struct Node *matter) {
+    if (matter != nullptr) {
+        cout << matter->data << "  ";
+        Preorder(matter->left);
+        Preorder(matter->right);
+    }
+}
+
+void Inorder(struct Node *matter) {
+    if (matter != nullptr) {
+        Inorder(matter->left);
+        cout << matter->data << "  ";
+        Inorder(matter->right);
+    }
+}
+
 int main() {
-    Solution matter;
-    vector<vector<int>> intervals = {{3,4}, {2,3}, {1,2}};
-    matter.findRightInterval(intervals);
+    struct Tree matter;
+    vector<int> contents = {4,1,6,0,2,5,7,3,8};
+    for (int num = 0; num < contents.size(); num++)
+        Create_AVL(matter.root, contents[num]);
+    Preorder(matter.root);  cout << endl;
+    Inorder(matter.root);   cout << endl;
     return 0;
 }
