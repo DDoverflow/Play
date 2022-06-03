b09d6ff02cc431042cbb843006aa9397ec35c789
StriveDD <Kip2021czb@gmail.com>
2022-06-03 11:10:09 +0800

    mofidy Task

--- Assignment/Task1.cpp
+++ Assignment/Task1.cpp
@@ -1,47 +1,88 @@
 #include <iostream>
-#include <cmath>
-#define PI 3.14
+#include <vector>
 using namespace std;
 
-// 作业第四题
-
-class Shape {
-public:
-    virtual double PrintArea() = 0;
+struct Node {
+    int data;
+    struct Node *left = nullptr;
+    struct Node *right = nullptr;
+    Node() {}
+    Node(int data): data(data) {}
 };
 
-class Circle: public Shape {
-public:
-    Circle(int data): r(data) {}
-    int r;
-    double PrintArea() {return PI * r * r;}
+struct Tree {
+    struct Node *root = nullptr;
 };
 
-class Rectangle: public Shape {
-public:
-    int x, y;
-    Rectangle(int x, int y): x(x), y(y) {}
-    double PrintArea() {return x * y;}
-};
-
-class Triangle: public Shape {
-public:
-    int a, b, c;
-    Triangle(int a, int b, int c): a(a), b(b), c(c) {}
-    double PrintArea() {
-        double C = (a + b + c) / 2;
-        return sqrt(C * (C - a) * (C - b) * (C - c));
+void Create_BST(struct Tree *tree, int data) {
+    struct Node *Addition = new Node(data);
+    if (tree->root == nullptr) {tree->root = Addition;  return;}
+    struct Node *middle = tree->root;
+    while (middle != nullptr) {
+        if (data >= middle->data) {
+            if (middle->right != nullptr) middle = middle->right;
+            else {middle->right = Addition;  return;}
+        }
+        else {
+            if (middle->left != nullptr) middle = middle->left;
+            else {middle->left = Addition;  return;}
+        }
     }
-};
+}
+
+void DeleteNode(struct Node *&matter) {
+    struct Node *front = matter, *rear;
+    if (matter->left == nullptr) {
+        matter = matter->left;
+        delete front;
+    }
+    else if (matter->right == nullptr) {
+        matter = matter->left;
+        delete front;
+    }
+    else {
+        rear = matter->left;
+        while (rear->right != nullptr) {
+            front = rear;
+            rear = rear->right;
+        }
+        matter->data = rear->data;
+        if (front == matter) front->left = rear->left;
+        else front->right = rear->left;
+        delete rear;
+    }
+}
+
+void FindNode(struct Node *&matter, int data) {
+    if (matter == nullptr) return;
+    if (data > matter->data) FindNode(matter->right, data);
+    else if (data < matter->data) FindNode(matter->left, data);
+    else DeleteNode(matter);
+}
+
+void Preorder(struct Node *root) {
+    if (root != nullptr) {
+        cout << root->data << "  ";
+        Preorder(root->left);
+        Preorder(root->right);
+    }
+}
+
+void Inorder(struct Node *root) {
+    if (root != nullptr) {
+        Inorder(root->left);
+        cout << root->data << "  ";
+        Inorder(root->right);
+    }
+}
 
 int main() {
-    Shape *matter = new Circle(2);
-    cout << "Circle: " << matter->PrintArea() << endl;
-    delete matter;
-    matter = new Rectangle(3, 4);
-    cout << "Rectangle: " << matter->PrintArea() << endl;
-    delete matter;
-    matter = new Triangle(3,4,5);
-    cout << "Triangle: " << matter->PrintArea() << endl;
+    struct Tree *matter = new Tree;
+    vector<int> contents = {5,3,6,2,4,7};
+    for (int num = 0; num < contents.size(); num++)
+        Create_BST(matter, contents[num]);
+    FindNode(matter->root, 7);
+    Preorder(matter->root);  cout << endl;
+    Inorder(matter->root);  cout << endl;
     return 0;
 }
--- Assignment/Task2.cpp
+++ Assignment/Task2.cpp
@@ -1,67 +1,28 @@
 #include <iostream>
-#define PI 3.14
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
 #include <cmath>
-#define len 5
 using namespace std;
 
-// 作业第五题
-
-class Shape {
+class Solution {
 public:
-    virtual double PrintArea() = 0;
-};
-
-class Circle: public Shape {
-public:
-    Circle(int data): r(data) {}
-    int r;
-    double PrintArea() {return PI * r * r;}
-};
-
-class Rectangle: public Shape {
-public:
-    int x, y;
-    Rectangle(int x, int y): x(x), y(y) {}
-    double PrintArea() {return x * y;}
-};
-
-class Triangle: public Shape {
-public:
-    int a, b, c;
-    Triangle(int a, int b, int c): a(a), b(b), c(c) {}
-    double PrintArea() {
-        double C = (a + b + c) / 2;
-        return sqrt(C * (C - a) * (C - b) * (C - c));
+    bool canConstruct(string ransomNote, string magazine) {
+        if (ransomNote.length() > magazine.length()) return false;
+        unordered_map<char, int> map;
+        for (int num = 0; num < magazine.length(); num++) map[magazine[num]]++;
+        for (int num = 0; num < ransomNote.length(); num++) {
+            if (map.find(ransomNote[num]) == map.end()) return false;
+            map[ransomNote[num]]--;
+            if (map[ransomNote[num]] == 0) map.erase(ransomNote[num]);
+        }
+        return true;
     }
 };
 
-class Square: public Shape {
-public:
-    int data;
-    Square(int data): data(data) {}
-    double PrintArea() {return pow(data, 2);}
-};
-
-class Trapezoid: public Shape {
-public:
-    int up, down, height;
-    Trapezoid(int up, int down, int height): up(up), down(down), height(height) {}
-    double PrintArea() {return (up + down) * height / 2;}
-};
-
 int main() {
-    Shape **matter = new Shape*[len];
-    matter[0] = new Circle(2);
-    matter[1] = new Rectangle(2, 3);
-    matter[2] = new Triangle(3,4,5);
-    matter[3] = new Square(5);
-    matter[4] = new Trapezoid(2,3,4);
-    cout << "Circle: " << matter[0]->PrintArea() << endl;
-    cout << "Rectangle: " << matter[1]->PrintArea() << endl;
-    cout << "Triangle: " << matter[2]->PrintArea() << endl;
-    cout << "Square: " << matter[3]->PrintArea() << endl;
-    cout << "Trapezoid: " << matter[4]->PrintArea() << endl;
-    double sum = 0;
-    for (int num = 0; num < len; num++) sum += matter[num]->PrintArea();
-    cout << "Sum: " << sum << endl;
+    Solution matter;
+    string ransomNode = "aa", magazine = "ab";
+    cout << "consist: " << matter.canConstruct(ransomNode, magazine) << endl;
+    return 0;
 }
