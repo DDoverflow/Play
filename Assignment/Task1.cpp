5f12efafe1299f8d7135da98b5172b3c9fdb40f8
StriveDD <Kip2021czb@gmail.com>
2022-06-24 17:37:27 +0800

    Modify Task

--- Assignment/Task1.cpp
+++ Assignment/Task1.cpp
@@ -2,87 +2,34 @@
 #include <vector>
 using namespace std;
 
-struct Node {
-    int data;
-    struct Node *left = nullptr;
-    struct Node *right = nullptr;
-    Node() {}
-    Node(int data): data(data) {}
+class Solution {
+public:
+	void Swap(vector<int> &nums, int left, int right) {
+		int temp = nums[left];
+		nums[left] = nums[right];
+		nums[right] = temp;
+	}
+
+	int Partition(vector<int> &nums, int front, int rear) {
+		while (front < rear) {
+			while (front < rear && nums[front] <= nums[rear]) rear--;
+			if (front < rear) Swap(nums, front, rear);
+			while (front < rear && nums[front] < nums[rear]) front++;
+			if (front < rear) Swap(nums, front, rear);
+		}
+		return front;
+	}
+
+	void QuickSort(vector<int> &nums, int front, int rear) {
+		if (front >= rear) return;
+		int middle = Partition(nums, front, rear);
+        QuickSort(nums, front, middle - 1);
+        QuickSort(nums, middle + 1, rear);
+	}
 };
-
-struct Tree {
-    struct Node *root = nullptr;
-};
-
-void Create_BST(struct Tree *tree, int data) {
-    struct Node *Addition = new Node(data);
-    if (tree->root == nullptr) {tree->root = Addition;  return;}
-    struct Node *middle = tree->root;
-    while (middle != nullptr) {
-        if (data >= middle->data) {
-            if (middle->right != nullptr) middle = middle->right;
-            else {middle->right = Addition;  return;}
-        }
-        else {
-            if (middle->left != nullptr) middle = middle->left;
-            else {middle->left = Addition;  return;}
-        }
-    }
-}
-
-void DeleteNode(struct Node *&matter) {
-    struct Node *front = matter, *rear;
-    if (matter->left == nullptr) {
-        matter = matter->left;
-        delete front;
-    }
-    else if (matter->right == nullptr) {
-        matter = matter->left;
-        delete front;
-    }
-    else {
-        rear = matter->left;
-        while (rear->right != nullptr) {
-            front = rear;
-            rear = rear->right;
-        }
-        matter->data = rear->data;
-        if (front == matter) front->left = rear->left;
-        else front->right = rear->left;
-        delete rear;
-    }
-}
-
-void FindNode(struct Node *&matter, int data) {
-    if (matter == nullptr) return;
-    if (data > matter->data) FindNode(matter->right, data);
-    else if (data < matter->data) FindNode(matter->left, data);
-    else DeleteNode(matter);
-}
-
-void Preorder(struct Node *root) {
-    if (root != nullptr) {
-        cout << root->data << "  ";
-        Preorder(root->left);
-        Preorder(root->right);
-    }
-}
-
-void Inorder(struct Node *root) {
-    if (root != nullptr) {
-        Inorder(root->left);
-        cout << root->data << "  ";
-        Inorder(root->right);
-    }
-}
 
 int main() {
-    struct Tree *matter = new Tree;
-    vector<int> contents = {5,3,6,2,4,7};
-    for (int num = 0; num < contents.size(); num++)
-        Create_BST(matter, contents[num]);
-    FindNode(matter->root, 7);
-    Preorder(matter->root);  cout << endl;
-    Inorder(matter->root);  cout << endl;
-    return 0;
+	string a = "abc", b = "abc";
+	cout << "Equal: " << (a == b) << endl;
+	return 0;
 }
--- Assignment/Task2.cpp
+++ Assignment/Task2.cpp
@@ -1,28 +1,107 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
-#include <unordered_set>
-#include <cmath>
+#include <stack>
+
 using namespace std;
 
-class Solution {
-public:
-    bool canConstruct(string ransomNote, string magazine) {
-        if (ransomNote.length() > magazine.length()) return false;
-        unordered_map<char, int> map;
-        for (int num = 0; num < magazine.length(); num++) map[magazine[num]]++;
-        for (int num = 0; num < ransomNote.length(); num++) {
-            if (map.find(ransomNote[num]) == map.end()) return false;
-            map[ransomNote[num]]--;
-            if (map[ransomNote[num]] == 0) map.erase(ransomNote[num]);
-        }
-        return true;
-    }
+struct Node {
+    int data;
+    struct Node *left = nullptr;
+    struct Node *right = nullptr;
+    Node() {}
+    Node(int data): data(data) {}
 };
 
+struct Tree {
+    struct Node *root = nullptr;
+};
+
+struct Node* CreateBinaryTree(struct Tree *tree, vector<int> &contents) {
+    vector<struct Node*> result(contents.size(), nullptr);
+    for (int number = 0; number < contents.size(); number++) {
+        if (contents[number] == -1) continue;
+        struct Node *Content = new Node(contents[number]);
+        result[number] = Content;
+    }
+    tree->root = result[0];
+    for (int current = 0; current < result.size(); current++) {
+        if (current * 2 + 1 < result.size()) result[current]->left = result[current * 2 + 1];
+        else break;
+        result[current]->right = result[current * 2 + 2];
+    }
+    return tree->root;
+}
+
+void Preorder(struct Node *content) {
+    if (content != nullptr) {
+        cout << content->data << "  ";
+        Preorder(content->left);
+        Preorder(content->right);
+    }
+}
+
+void Inorder(struct Node *content) {
+    if (content != nullptr) {
+        Inorder(content->left);
+        cout << content->data << "  ";
+        Inorder(content->right);
+    }
+}
+
+void Preorder(struct Node *root, vector<int> &result) {
+    stack<struct Node *> Stack;
+    if (root == nullptr) return;
+    Stack.push(root);
+    while (!Stack.empty()) {
+        struct Node *content = Stack.top();
+        Stack.pop();
+        result.push_back(content->data);
+        if (content->right) Stack.push(content->right);
+        if (content->left) Stack.push(content->left);
+    }
+}
+
+void Inorder(struct Node *root, vector<int> &result) {
+    stack<struct Node *> Stack;
+    if (root == nullptr) return;
+    while (root != nullptr || !Stack.empty()) {
+        if (root != nullptr) {
+            Stack.push(root);
+            root = root->left;
+        }
+        else {
+            root = Stack.top();
+            Stack.pop();
+            result.push_back(root->data);
+            root = root->right;
+        }
+    }
+}
+
+void Postorder(struct Node *root, vector<int> &result) {
+    stack<struct Node *> Stack;
+    if (root == nullptr) return;
+    Stack.push(root);
+    while (!Stack.empty()) {
+        struct Node *content = Stack.top();
+        Stack.pop();
+        result.push_back(content->data);
+        if (content->left) Stack.push(content->left);
+        if (content->right) Stack.push(content->right);
+    }
+    reverse(result.begin(), result.end());
+}
+
 int main() {
-    Solution matter;
-    string ransomNode = "aa", magazine = "ab";
-    cout << "consist: " << matter.canConstruct(ransomNode, magazine) << endl;
+    struct Tree *tree = new Tree;
+    vector<int> contents = {4,1,6,0,2,5,7,-1,-1,-1,3,-1,-1,-1,8};
+    CreateBinaryTree(tree, contents);
+    vector<int> result;
+    Postorder(tree->root, result);
+    for (int num = 0; num < result.size(); num++)
+        cout << result[num] << "  ";
+    cout << endl;
     return 0;
 }
+
+// 4  1  0  2  3  6  5  7  8
